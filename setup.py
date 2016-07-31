5c7e1984967c9a8bdf7752b96727e536dd23c95d
Zachary Stewart <zachary@zstewart.com>
2016-07-31 16:02:05 -0400

    added setup script

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+def readme():
+  with open('README.md') as rm:
+    return rm.read()
+
+setup(
+    name='stuff',
+    version='0.0.1',
+    description='A library for keeping track of quantities of stuff',
+    long_description=readme(),
+    url='http://github.com/zstewar1/stuff',
+    author='Zachary Stewart',
+    author_email='zachary@zstewart.com',
+    packages=['stuff'],
+    test_suite='stuff.tests',
+)
